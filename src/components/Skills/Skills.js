6a598001c80b580b4ff1b6b8d6ffe252fa375095
[supraditya] <[aditya300100@gmail.com]>
2021-02-07 18:59:33 +0530

    Timeline added

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import EduContainer from './EduContainer/Educontainer';
+import Timeline from './Timeline/Timeline';
 import styles from './Skills.module.css';
 import {DiPhp, DiJqueryLogo, DiReact, DiPython, DiJava, DiIllustrator, DiBootstrap} from 'react-icons/di';
 import {SiJavascript, SiMysql, SiMicrosoftoffice} from 'react-icons/si';
@@ -16,10 +17,10 @@ const Skills=()=>{
                     <span className={styles.skillIcon}><DiReact color="#61DAFB"/></span>
                     <span className={styles.skillIcon}><DiPython color="#3776AB"/></span>
                     <span className={styles.skillIcon}><DiJava color="#007396"/></span>
-                    <span className={styles.skillIcon}><img src={require('../../assets/cLang.svg')} /></span>
+                    <span className={styles.skillIcon}><img src={require('../../assets/cLang.svg')} alt="C logo" /></span>
                     <span className={styles.skillIcon}><DiIllustrator color="#FF9A00"/></span>
                     <span className={styles.skillIcon}><DiBootstrap color="#7952B3"/></span>
-                    <span className={styles.skillIcon}><img src={require('../../assets/Figma.svg')} /></span>
+                    <span className={styles.skillIcon}><img src={require('../../assets/Figma.svg')} alt="figma logo" /></span>
                     <span className={styles.skillIcon}><SiJavascript color="#F7DF1E"/></span>
                     <span className={styles.skillIcon}><SiMysql color="#4479A1"/></span>
                     <span className={styles.skillIcon}><SiMicrosoftoffice color="#D83B01"/></span>
@@ -31,6 +32,10 @@ const Skills=()=>{
                     <EduContainer start="2016" end="2018" fact="Computer Science: 183/200" instName="PACE Junior Science College"/>
                     <EduContainer start="2009" end="2016" fact="Computer Science: 100/100" instName="Smt. Sulochanadevi Singhania School"/>
                 </div>
+            </div>
+            <div className={styles.rightDiv}>
+                <h1 className={styles.whiteText}>Experience</h1>
+                <Timeline/>
             </div>
         </div>
     )
