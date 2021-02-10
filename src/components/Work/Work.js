b7bee12f8ff3910fde1771d3f86ff7f081ba1dc1
[supraditya] <[aditya300100@gmail.com]>
2021-02-10 23:21:18 +0530

    Minor content updates

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -7,14 +7,19 @@ const Work=()=>{
             <div className={styles.whiteText}>
                 My Work
             </div>
+            <WorkContainer
+                title="HackBattle '20"
+                desc="Landing Page for Hackbattle '20, a 36-Hour Multidisciplinary Hackathon. Hosted at hackbattle.ieeecsvit.com"
+                link="https://github.com/IEEECS-VIT/HackBattle20"
+            />
             <WorkContainer 
             title="Huntr" 
             desc="Huntr is the outcome of a combat robotics project, developed as an entry for the Robowars tournament organized in VIT, Vellore by RoboVITics, in the 30kg category." 
             link="https://github.com/supraditya/Huntr"/>
             <WorkContainer
-                title="HackBattle '20"
-                desc="Landing Page for Hackbattle '20, a 36-Hour Multidisciplinary Hackathon. Hosted at hackbattle.ieeecsvit.com"
-                link="https://github.com/IEEECS-VIT/HackBattle20"
+            title="Surya NestBuild"
+            desc="Redesigned views for the Surya Nestbuild Ltd.'s website."
+            link="https://github.com/supraditya/suryaRealEstate"
             />
         </div>
 
