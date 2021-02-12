3204d4dc29a07a6dacae2d25cfbefd703ba32ab1
[supraditya] <[aditya300100@gmail.com]>
2021-02-12 13:18:35 +0530

    Sidebar button animation complete

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,20 +4,43 @@ import styles from './Sidebar.module.css';
 
 class Sidebar extends Component{
     state={
+        buttonStyle:"",
+        line1StyleColor: "",
+        line1StyleAngle: "",
+        // line1StyleTranslate: "",
+        line2StyleColor: "",
+        line2StyleAngle: "",
+        // line2StyleTranslate: "",
         sidebarCollapsed: true,
         sidebar: "",
     }
     sidebarHandler=()=>{
         if(this.state.sidebarCollapsed)
         {
+            // Sidebar opening
             this.setState({
+                buttonStyle: "#000",
+                line1StyleColor: "#fff",
+                line1StyleAngle: "rotate(-45deg)translate(-2px, 6.5px)",
+                // line1StyleTranslate: "translate(-5px, -6px)",
+                line2StyleColor: "#fff",
+                line2StyleAngle: "rotate(45deg)translate(-2px,-6.5px)",
+                // line2StyleTranslate: "translate(-5px, 6px)",
                 sidebarCollapsed: false,
                 sidebar: "translate(250px, 0)",
             });
         }
         else
         {
+            // Sidebar closing
             this.setState({
+                buttonStyle: "#fff",
+                line1StyleColor: "#000",
+                line1StyleAngle: "rotate(0deg)translate(0px, 0px)",
+                // line1StyleTranslate: "translate(0px, 0px)",
+                line2StyleColor: "#000",
+                line2StyleAngle: "rotate(0deg)translate(0px, 0px)",
+                // line2StyleTranslate: "translate(0px, 0px)",
                 sidebarCollapsed: true,
                 sidebar: "translate(500px, 0)",
             });
@@ -28,9 +51,9 @@ class Sidebar extends Component{
     {
         return(
             <div style={{transform: this.state.sidebar}} className={styles.outer}>
-                <button onClick={()=>this.sidebarHandler()} className={styles.navButton}>
-                    <div className={styles.line1}></div>
-                    <div className={styles.line2}></div>
+                <button style={{backgroundColor: this.state.buttonStyle}} onClick={()=>this.sidebarHandler()} className={styles.navButton}>
+                    <div style={{borderColor: this.state.line1StyleColor, transform: this.state.line1StyleAngle}} className={styles.line1}></div>
+                    <div style={{borderColor: this.state.line2StyleColor, transform: this.state.line2StyleAngle}} className={styles.line2}></div>
                 </button>
                 <div className={styles.sidebarOuter}>
                     {this.state.sidebarCollapsed}
