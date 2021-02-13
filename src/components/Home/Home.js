84415fc005abe3f04325dd470e0609cbcf9a81b9
[supraditya] <[aditya300100@gmail.com]>
2021-02-13 22:19:15 +0530

    Sidebar changes, home view fully responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,42 +6,38 @@ import scrollTo from 'gatsby-plugin-smoothscroll';
 const Home=()=>{
     return(
         <div className={styles.home}>
-        <div>
-        <p className={styles.whiteText}>Hi, I'm <br></br><span className={styles.orangeTextLarge}>Aditya Menon</span></p>
-        <p className={styles.whiteTextLarge}>Frontend Web Developer</p>
-        <div className={styles.chevDiv}>
-          <button onClick={() => scrollTo('#about')}><span className={styles.icon}><FaChevronDown size="70px"/></span></button>
-        </div>
+        <div className={styles.textDiv}>
+          <p className={styles.whiteText}>Hi, I'm <br></br><span className={styles.orangeTextLarge}>Aditya Menon</span></p>
+          <p className={styles.whiteTextLarge}>Frontend Web Developer</p>
+          <div className={styles.chevDiv}>
+            <button onClick={() => scrollTo('#about')}><span className={styles.icon}><FaChevronDown size="70px"/></span></button>
+          </div>
         </div>
         <div className={styles.actionButtons}>
           <div className={styles.linkButtons}>
-          {/* <a href="tel:7738014883">
-            <span className={styles.icon}>
-            <FaPhoneAlt size="45px"/>
-            </span>
-          </a> */}
-          <a href="https://www.linkedin.com/in/supraditya" target="_blank" rel="noreferrer">
-            <span className={styles.icon}>
-              <FaLinkedin size="50px"/>
-            </span>
-          </a>
-          <a href="https://www.github.com/supraditya" target="_blank" rel="noreferrer">
-            <span className={styles.icon}>
-              <FaGithub size="50px"/>
-            </span>
-          </a>
-          <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=aditya300100@gmail.com&tf=1" target="_blank" rel="noreferrer">
-            <span className={styles.icon}>
-              <IoMail size='55px'/>
-            </span>
-          </a>
-          </div>
-          <a 
-          href="https://drive.google.com/file/d/1QuiHhxFY7Xvqwhe3znrNz9iwJAqafh7A/view?usp=sharing" 
-          target="_blank" rel="noreferrer" className={styles.buttonStyle}>
-            <FaGoogleDrive size='34px' />&ensp;VIEW RESUME
-          </a>
+            <a href="https://www.linkedin.com/in/supraditya" target="_blank" rel="noreferrer">
+              <span className={styles.icon}>
+                <FaLinkedin/>
+              </span>
+            </a>
+            <a href="https://www.github.com/supraditya" target="_blank" rel="noreferrer">
+              <span className={styles.icon}>
+                <FaGithub/>
+              </span>
+            </a>
+            <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=aditya300100@gmail.com&tf=1" target="_blank" rel="noreferrer">
+              <span className={styles.icon}>
+                <IoMail/>
+              </span>
+            </a>
+            </div>
+            <a 
+            href="https://drive.google.com/file/d/1QuiHhxFY7Xvqwhe3znrNz9iwJAqafh7A/view?usp=sharing" 
+            target="_blank" rel="noreferrer" className={styles.buttonStyle}>
+              <FaGoogleDrive/>&ensp;VIEW RESUME
+            </a>
         </div>
+        <button className={styles.mobileChevDiv} onClick={() => scrollTo('#about')}><span className={styles.icon}><FaChevronDown size="50px"/></span></button>
       </div>
     )
 }
