8924cb9b06bd7980911b0f981d45b5d91e53d654
[supraditya] <[aditya300100@gmail.com]>
2021-02-13 22:57:29 +0530

    about fully responsive

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,9 +11,26 @@ const About=()=>{
                 <p className={styles.whiteText}>
                 I, <span className={styles.orangeText}>Aditya Menon</span>, am a 3rd Year Computer Science student here at VIT Vellore.<br></br><br></br> I have been working as a self-taught Frontend Web Developer and Designer for <span className={styles.orangeText}>14</span> months and have completed an internship, along with several projects for my Student Organisation: <span className={styles.orangeText}>IEEE-CS VIT</span>.<br></br><br></br> Apart from Web Development, I occasionally produce music, sketch comedy and standups since you either gotta be funny or good-looking (I’m neither)
                 </p>
+                <div className={styles.iconDivMobile}>
+                    <a href="https://www.instagram.com/menon_aa_gaya/" target="_blank" rel="noreferrer">
+                        <span className={styles.icon}>
+                        <FaInstagram size="40px"/>
+                        </span>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UC4J1TSccU4G_CriLC9eoAQw" target="_blank" rel="noreferrer">
+                        <span className={styles.icon}>
+                        <FaYoutube size="40px"/>
+                        </span>
+                    </a>
+                    <a href="https://soundcloud.com/p-r-y-m-e-v-i-l" target="_blank" rel="noreferrer">
+                        <span className={styles.icon}>
+                        <FaSoundcloud size="40px"/>
+                        </span>
+                    </a>
+                </div>
                 <div className={styles.chevDiv}>
                     <p className={styles.thinWhiteText}>NOW, ONTO BUSINESS</p>
-                    <button onClick={() => scrollTo('#skills')}><span className={styles.icon}><FaChevronDown size="70px"/></span></button>
+                    <button onClick={() => scrollTo('#skills')}><span className={styles.icon}><FaChevronDown/></span></button>
                 </div>
             </div>
             <div className={styles.aboutRight}>
