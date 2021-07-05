f12369c06d34e128bb51b56969f5306afc9d1596
[supraditya] <[aditya300100@gmail.com]>
2021-07-05 20:13:11 +0530

    added react-helment plugin in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     //   },
     // },
     "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
