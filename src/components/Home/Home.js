2f95d030d7a9a6f1337d503c22f8ff338a04b264
Aditya Menon <43109943+supraditya@users.noreply.github.com>
2021-11-01 21:08:53 +0530

    updated resume link to reflect portfolio website link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -60,7 +60,7 @@ const Home = () => {
           </a>
         </div>
         <a
-          href="https://drive.google.com/file/d/1d2sI8_dT6HxT_OWWov0HAagt_RgX1kE7/view?usp=sharing"
+          href="https://drive.google.com/file/d/1eKnxgz8qyZdUKKeiTkHy_gALA1S1GQX1/view?usp=sharing"
           target="_blank"
           rel="noreferrer"
           className={styles.buttonStyle}
