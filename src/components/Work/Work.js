916579eccdff7c6000331e8db7e5b5c4e1c0798d
[supraditya] <[aditya300100@gmail.com]>
2021-12-09 22:40:01 +0530

    Basic project card added

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,18 +1,25 @@
 import React from "react";
 import styles from "./Work.module.css";
-import WorkContainer from "./WorkContainer/WorkContainer";
-import WorkContainerSite from "./WorkContainer-siteLink/WorkContainerSite";
+// import WorkContainer from "./WorkContainer/WorkContainer";
+import ProjectCard from "./ProjectCard/ProjectCard";
+// import WorkContainerSite from "./WorkContainer-siteLink/WorkContainerSite";
 const Work = () => {
   return (
     <div className={styles.work}>
       <div className={styles.whiteText}>My Work</div>
-      <WorkContainerSite
+      <br></br>
+      {/* <WorkContainerSite
         title="IEEECS-VIT Website"
         desc="Landing Page for IEEECS-VIT, a technical Student Organisation operating from VIT Vellore. Developed using GatsbyJS. Hosted at ieeecsvit.com"
         link="https://github.com/IEEECS-VIT/chapter-website-2021"
         link2="https://www.ieeecsvit.com"
+      /> */}
+      <ProjectCard
+        title="IEEECS-VIT Website"
+        desc="Landing Page for IEEECS-VIT"
+        domains={['Web Development', 'Project Management']}
       />
-      <WorkContainerSite
+      {/* <WorkContainerSite
         title="HackBattle '20"
         desc="Landing Page for Hackbattle '20, a 36-Hour Multidisciplinary Hackathon. Hosted at hackbattle.ieeecsvit.com"
         link="https://github.com/IEEECS-VIT/HackBattle20"
@@ -38,7 +45,7 @@ const Work = () => {
         title="Fire Detection System"
         desc="A CNN Model trained on a dataset of images containing fire, using our pre-processing algorithm that increases real-time flame detection accuracy by almost 12%"
         link="https://github.com/supraditya/FireDetector"
-      />
+      /> */}
     </div>
   );
 };
