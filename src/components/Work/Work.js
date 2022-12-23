9c7757024fe019957a195f95de4a53713ab6c282
supraditya <aditya300100@gmail.com>
2022-12-23 18:04:32 -0500

    added 'see more' button for projects

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Work.module.css";
-// import img from "../../assets/projects/ieeecsvit_ss.png";
-// import WorkContainer from "./WorkContainer/WorkContainer";
 import ProjectCard from "./ProjectCard/ProjectCard";
 import { FaChevronDown } from "react-icons/fa";
 import scrollTo from "gatsby-plugin-smoothscroll";
 // import WorkContainerSite from "./WorkContainer-siteLink/WorkContainerSite";
 const Work = (props) => {
+  const [seeMoreState, setseeMoreState] = useState(false);
   return (
     <div className={styles.work}>
       <div className={styles.whiteText}>My Work</div>
@@ -27,8 +26,10 @@ const Work = (props) => {
           modalContent={{
             "The Problem":
               "Household food wastage has been a significant problem globally. According to the American Journal of Agricultural Economics, the average American household ends up wasting up to 31.9% of the food they purchase! This is caused in part due to the amount of mental effort required in keeping track of household food inventory so that everything is used up before it expires and the amount of time and effort it takes to meal-plan and determine the dish that tastes good, is easy to prepare, and uses an adequate amount of ingredients. Thus, we wondered how we might help people to safely consume all the highly perishable foods in their pantry without wastage, while simultaneously alleviating them of some of the mental burden of managing a household?",
-            "Our Solution": "This conundrum led us to come up with Forgetful Foodie: a smartphone application designed to help users consume all their perishable food items before they go bad through individual item age tracking and by suggesting appropriate recipes to tactfully use at-risk ingredients!",
-            "My Role": "I worked on this Interaction Design with two other teammates. My teammates were focused on the UX Research aspect of the project, while I was the primary UX Designer and developed the majority of the prototype after all feedback was interpreted and consolidated by the team as a whole.",
+            "My Role":
+              "I worked on this Interaction Design with two other teammates. My teammates were focused on the UX Research aspect of the project, while I was the primary UX Designer and developed the majority of the prototype after all feedback was interpreted and consolidated by the team as a whole.",
+            "Our Solution":
+              "This conundrum led us to come up with Forgetful Foodie: a smartphone application designed to help users consume all their perishable food items before they go bad through individual item age tracking and by suggesting appropriate recipes to tactfully use at-risk ingredients!",
           }}
         />
         <ProjectCard
@@ -45,8 +46,8 @@ const Work = (props) => {
           setbodyScrollState={props.setbodyScrollState}
           modalContent={{
             "The Problem": "Lorem ipsum 1",
-            "Our Solution": "Lorem ipsum 2",
-            "My Role": "Lorem Ipsum 3",
+            "My Role": "Lorem Ipsum 2",
+            "Our Solution": "Lorem ipsum 3",
           }}
         />
         <ProjectCard
@@ -58,8 +59,8 @@ const Work = (props) => {
           setbodyScrollState={props.setbodyScrollState}
           modalContent={{
             "The Problem": "Lorem ipsum 1",
-            "Our Solution": "Lorem ipsum 2",
-            "My Role": "Lorem Ipsum 3",
+            "My Role": "Lorem Ipsum 2",
+            "Our Solution": "Lorem ipsum 3",
           }}
         />
         <ProjectCard
@@ -71,38 +72,57 @@ const Work = (props) => {
           setbodyScrollState={props.setbodyScrollState}
           modalContent={{
             "The Problem": "Lorem ipsum 1",
-            "Our Solution": "Lorem ipsum 2",
-            "My Role": "Lorem Ipsum 3",
+            "My Role": "Lorem Ipsum 2",
+            "Our Solution": "Lorem ipsum 3",
           }}
         />
-        <ProjectCard
-          title="Dusk Security"
-          domains={["Logo Design", "Web Design", "Web Development"]}
-          tech_used={["GatsbyJS", "React", "Netlify", "Figma", "Illustrator"]}
-          designLink="https://www.figma.com/file/v5KjDGjjFHnPV9w22uRkVt/Dusk-Security-Landing-Page?node-id=302%3A1582"
-          siteLink="https://dusksecurity.com"
-          project_img="dusksecurity"
-          setbodyScrollState={props.setbodyScrollState}
-          modalContent={{
-            "The Problem": "Lorem ipsum 1",
-            "Our Solution": "Lorem ipsum 2",
-            "My Role": "Lorem Ipsum 3",
-          }}
-        />
-        <ProjectCard
-          title="IEEECS-VIT Website"
-          domains={["Web Development", "Project Management"]}
-          tech_used={["GatsbyJS", "React", "Netlify"]}
-          gitLink="https://github.com/IEEECS-VIT/chapter-website-2021"
-          siteLink="https://www.ieeecsvit.com"
-          project_img="ieeecsvit"
-          setbodyScrollState={props.setbodyScrollState}
-          modalContent={{
-            "The Problem": "Lorem ipsum 1",
-            "Our Solution": "Lorem ipsum 2",
-            "My Role": "Lorem Ipsum 3",
-          }}
-        />
+        <div className={styles.seeMoreContainer}>
+          <button
+            className={styles.actionButtons}
+            onClick={() => setseeMoreState(!seeMoreState)}
+          >SEE&nbsp;{seeMoreState ? <span>LESS</span> : <span>MORE</span>}&nbsp;PROJECTS
+          </button>
+        </div>
+        {seeMoreState ? (
+          <div className={styles.moreProjectsContainer}>
+            <ProjectCard
+              title="Dusk Security"
+              domains={["Logo Design", "Web Design", "Web Development"]}
+              tech_used={[
+                "GatsbyJS",
+                "React",
+                "Netlify",
+                "Figma",
+                "Illustrator",
+              ]}
+              designLink="https://www.figma.com/file/v5KjDGjjFHnPV9w22uRkVt/Dusk-Security-Landing-Page?node-id=302%3A1582"
+              siteLink="https://dusksecurity.com"
+              project_img="dusksecurity"
+              setbodyScrollState={props.setbodyScrollState}
+              modalContent={{
+                "The Problem": "Lorem ipsum 1",
+                "My Role": "Lorem Ipsum 2",
+                "Our Solution": "Lorem ipsum 3",
+              }}
+            />
+            <ProjectCard
+              title="IEEECS-VIT Website"
+              domains={["Web Development", "Project Management"]}
+              tech_used={["GatsbyJS", "React", "Netlify"]}
+              gitLink="https://github.com/IEEECS-VIT/chapter-website-2021"
+              siteLink="https://www.ieeecsvit.com"
+              project_img="ieeecsvit"
+              setbodyScrollState={props.setbodyScrollState}
+              modalContent={{
+                "The Problem": "Lorem ipsum 1",
+                "My Role": "Lorem Ipsum 2",
+                "Our Solution": "Lorem ipsum 3",
+              }}
+            />
+          </div>
+        ) : (
+          <span></span>
+        )}
         {/* <ProjectCard
           title="MOR TechFab"
           domains={["Web Development", "Project Management"]}
@@ -110,7 +130,7 @@ const Work = (props) => {
           siteLink="https://www.mortechfab.com"
           project_img="mortechfab"
         /> */}
-        <ProjectCard
+        {/* <ProjectCard
           title="Hackbattle '20"
           domains={["Web Development"]}
           tech_used={["JQuery", "FullPageJS", "Netlify"]}
@@ -119,10 +139,10 @@ const Work = (props) => {
           setbodyScrollState={props.setbodyScrollState}
           modalContent={{
             "The Problem": "Lorem ipsum 1",
-            "Our Solution": "Lorem ipsum 2",
-            "My Role": "Lorem Ipsum 3",
+            "My Role": "Lorem Ipsum 2",
+            "Our Solution": "Lorem ipsum 3",
           }}
-        />
+        /> */}
         {/* <ProjectCard
           title="Huntr"
           domains={["Combat Robotics"]}
@@ -137,7 +157,7 @@ const Work = (props) => {
           gitLink="https://github.com/supraditya/FireDetector"
           project_img="github_placeholder"
         /> */}
-        <ProjectCard
+        {/* <ProjectCard
           title="Portfolio Website"
           domains={["Web Design, Web Development"]}
           tech_used={["GatsbyJS", "React", "Figma", "Illustrator", "Netlify"]}
@@ -148,10 +168,10 @@ const Work = (props) => {
           setbodyScrollState={props.setbodyScrollState}
           modalContent={{
             "The Problem": "Lorem ipsum 1",
-            "Our Solution": "Lorem ipsum 2",
-            "My Role": "Lorem Ipsum 3",
+            "My Role": "Lorem Ipsum 2",
+            "Our Solution": "Lorem ipsum 3",
           }}
-        />
+        /> */}
       </div>
       <div className={styles.chevDiv}>
         <p className={styles.thinWhiteText}>WHAT I CAN DO</p>
